5b04ff680f6d7c554c86b4a45b8959bc7c22b6b9
aaronisme <aarondongchen@gmail.com>
2022-07-26 18:32:52 +0800

    change build path

--- build.rs
+++ build.rs
@@ -14,13 +14,13 @@ fn main() {
     let out_src = Path::new(&out_dir).join("java_glue.rs");
     Generator::new(TypeCases::CamelCase, Language::Java, "src").generate_interface(in_src);
     //delete the lib folder then create it again to prevent obsolete files from staying
-    let java_folder = Path::new("../src/main/java/com/keystone/cold/cryptocore");
+    let java_folder = Path::new("../src/main/java/com/keystone/cold/cryptocore/lib");
     if java_folder.exists() {
         fs::remove_dir_all(java_folder);
     }
     fs::create_dir(java_folder).unwrap();
     let swig_gen = flapigen::Generator::new(LanguageConfig::JavaConfig(
-        JavaConfig::new(java_folder.into(), "com.keystone.cold.lib".into())
+        JavaConfig::new(java_folder.into(), "com.keystone.cold.cryptocore.lib".into())
             .use_null_annotation_from_package("androidx.annotation".into()),
     ))
     .rustfmt_bindings(true);
